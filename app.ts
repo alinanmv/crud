3ff6312648822dfd7658bc8ba80170bffc7c50a3
alinanmv <alinanumanova555@gmail.com>
2026-08-04 15:53:38 +0500

    routes & controllers

--- app.ts
+++ app.ts
@@ -3,10 +3,13 @@ dotenv.config({ path: "./atlas-credentials.env" });
 
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-import Product from "./models/product.model";
+import productRoutes from "./routes/product.route";
 
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/products", productRoutes);
 
 app.listen(3000, () => {
   console.log(`App listening on port 3000`);
@@ -14,65 +17,6 @@ app.listen(3000, () => {
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
-});
-
-app.get("/api/products", async (req: Request, res: Response) => {
-  try {
-    const products = await Product.find();
-    res.status(200).json(products);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-});
-
-app.get("/api/product/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-});
-
-app.post("/api/products", async (req: Request, res: Response) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-});
-
-app.put("/api/product/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-
-    if (!product) {
-      res.status(404).json({ error: "Product not found" });
-    }
-
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-});
-
-app.delete("/api/product/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-
-    if (!product) {
-      res.status(404).json({ error: "Product not found" });
-    }
-
-    res.status(200).json({ message: "product deleted successfully" });
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
 });
 
 mongoose
---
+++ controllers/product.controller.ts
@@ -0,0 +1,63 @@
+import e, { Request, Response } from "express";
+import Product from "../models/product.model";
+
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const getProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const addProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json({ message: "product deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
